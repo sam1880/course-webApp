86ee8bee2c7cce9cc099b0f061847c6b56687aa1
sam1880 <chouhanharshit87@gmail.com>
2023-07-18 12:32:47 +0530

    added routing

--- client/courseApp/src/App.jsx
+++ client/courseApp/src/App.jsx
@@ -1,14 +1,20 @@
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import Appbar from './components/Appbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
 
   return (
     <div>
-      <Appbar></Appbar>
-      <SignUp></SignUp>
+      <Router>
+        <Appbar/>
+        <Routes>
+          <Route path='/signin' element={<SignIn/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- client/courseApp/src/components/Appbar.jsx
+++ client/courseApp/src/components/Appbar.jsx
@@ -1,14 +1,16 @@
 import { Button } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import {useNavigate} from 'react-router-dom'
 
 function Appbar(){
+    const navigate = useNavigate()
 
     return(
         <div style={{display: "flex", justifyContent: "space-between"}}>
             <Typography>Course Shop</Typography>
             <div>
-                <Button>Sign In</Button>
-                <Button>Sign Up</Button>
+                <Button onClick={() => navigate('/SignIn')}>Sign In</Button>
+                <Button onClick={()=> navigate('/SignUp')}>Sign Up</Button>
             </div>
         </div>
     )
