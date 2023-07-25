935519d3d919235b68285d079c499f019dc9b90a
sam1880 <chouhanharshit87@gmail.com>
2023-07-25 13:32:24 +0530

    added different auth for users and admins so users wont get access to admin's page

--- backend/server.js
+++ backend/server.js
@@ -13,21 +13,28 @@ const PORT = 3000
 var COURSES = []
 var USERS = []
 var ADMINS = []
+  
+const secretKeyAdmin = "blahblah";
+const secretKeyUser = "blahblah1";
 
-const secretKey = "blahblah";
 
-const generateJwt = (user) =>{
+const generateJwtAdmin = (user) =>{
     const payload = {username: user.username}
-    return jwt.sign(payload, secretKey, {expiresIn: '1h'});
+    return jwt.sign(payload, secretKeyAdmin, {expiresIn: '1h'});
 };
 
-const authenticateJwt = (req, res, next) =>{
+const generateJwtUser = (user) =>{
+    const payload = {username: user.username}
+    return jwt.sign(payload, secretKeyUser, {expiresIn: '1h'});
+};
+
+const authenticateJwtAdmin = (req, res, next) =>{
     const authHeader = req.headers.authorization;
 
     if(authHeader){
         const token = authHeader.split(' ')[1];
 
-        jwt.verify(token,secretKey, (err,user) =>{
+        jwt.verify(token,secretKeyAdmin, (err,user) =>{
             if(err){
                 return res.sendStatus(403)
             }
@@ -42,19 +49,38 @@ const authenticateJwt = (req, res, next) =>{
 
 }
 
+const authenticateJwtUser = (req, res, next) =>{
+    const authHeader = req.headers.authorization;
 
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
 
-//ADMIN
-const adminAuthentication = (req, res, next) =>{
-    const {username, password} = req.headers;
-    const admin = ADMINS.find(a => a.username == username && a.password == password);
-    if(admin){
-        next();
+        jwt.verify(token,secretKeyUser, (err,user) =>{
+            if(err){
+                return res.sendStatus(403)
+            }
+            
+            req.user = user;
+            next();
+        })
     }
     else{
-        res.status(403).json({message: 'admin authentication faild'})
+        res.sendStatus(401);
     }
+
 }
+
+//ADMIN
+// const adminAuthentication = (req, res, next) =>{
+//     const {username, password} = req.headers;
+//     const admin = ADMINS.find(a => a.username == username && a.password == password);
+//     if(admin){
+//         next();
+//     }
+//     else{
+//         res.status(403).json({message: 'admin authentication faild'})
+//     }
+// }
 
 
 app.post("/admin/signup", (req, res) => {
@@ -65,7 +91,7 @@ app.post("/admin/signup", (req, res) => {
     } 
     else{
         ADMINS.push(admin);
-        const token = generateJwt(admin);
+        const token = generateJwtAdmin(admin);
         res.json({message: 'admin added successfully', token})
     }
 })
@@ -74,7 +100,7 @@ app.post("/admin/signin", (req, res) =>{
     const {username, password} = req.headers;
     const admin = ADMINS.find(a => a.username == username && a.password == password);
     if(admin){
-        const token = generateJwt(admin)
+        const token = generateJwtAdmin(admin)
         res.json({message: 'logged in successfully', token})
     }
     else{
@@ -82,14 +108,14 @@ app.post("/admin/signin", (req, res) =>{
     }
 })
 
-app.post("/admin/courses", authenticateJwt, (req, res) =>{
+app.post("/admin/courses", authenticateJwtAdmin, (req, res) =>{
     const course = req.body;
     course.id = Date.now();
     COURSES.push(course);
     res.json({message: 'course added successfully', courseID: course.id})
 })
 
-app.put("/admin/courses/:id", authenticateJwt, (req, res)=>{
+app.put("/admin/courses/:id", authenticateJwtAdmin, (req, res)=>{
     const courseId = parseInt(req.params.id)
     const course = COURSES.find(a => a.id === courseId);
     if(course){
@@ -101,22 +127,22 @@ app.put("/admin/courses/:id", authenticateJwt, (req, res)=>{
     }
 })
 
-app.get("/admin/courses", authenticateJwt, (req, res) =>{
+app.get("/admin/courses", authenticateJwtAdmin, (req, res) =>{
     res.json({courses: COURSES})
 })
 
 //USER
-const userAuthentication = (req,res, next) =>{
-    const {username, password} = req.headers;
-    const user = USERS.find(a => a.username == username && a.password == password);
-    if(user){
-        req.user = user;
-        next()
-    }
-    else{
-        res.json({message: 'user authentication failed'})
-    }
-}
+// const userAuthentication = (req,res, next) =>{
+//     const {username, password} = req.headers;
+//     const user = USERS.find(a => a.username == username && a.password == password);
+//     if(user){
+//         req.user = user;
+//         next()
+//     }
+//     else{
+//         res.json({message: 'user authentication failed'})
+//     }
+// }
 
 app.post("/user/signup", (req, res) =>{
     const user = {...req.body, purchasedCourses: []};
@@ -127,7 +153,7 @@ app.post("/user/signup", (req, res) =>{
     else{
         user.id = Date.now();
         USERS.push(user);
-        token = generateJwt(user)
+        token = generateJwtUser(user)
         res.json({message: 'user created successfully', token})
     }
 })
@@ -137,7 +163,7 @@ app.post("/user/signin", (req, res) =>{
     const user = USERS.find(a => a.username == username && a.password == password);
     if(user){
         req.user = user;
-        token = generateJwt(user)
+        token = generateJwtUser(user)
         res.json({message: 'user logged in successfully', token})
     }
     else{
@@ -145,11 +171,11 @@ app.post("/user/signin", (req, res) =>{
     }
 })
 
-app.get("/user/courses", authenticateJwt, (req, res) =>{
+app.get("/user/courses", authenticateJwtUser, (req, res) =>{
     res.json({courses: COURSES.filter(a => a.published)})
 })
 
-app.post("/user/courses/:id", authenticateJwt, (req,res)=>{
+app.post("/user/courses/:id", authenticateJwtUser, (req,res)=>{
     const courseId = Number(req.params.id);
     const course = COURSES.find(a => a.id == courseId && a.published);
     if(course){
@@ -161,7 +187,7 @@ app.post("/user/courses/:id", authenticateJwt, (req,res)=>{
     }
 })
 
-app.get("/user/purchasedcourses", authenticateJwt, (req, res) =>{
+app.get("/user/purchasedcourses", authenticateJwtUser, (req, res) =>{
     res.json({purchasedCourses: COURSES.filter(a=> req.user.purchasedCourses.includes(a.id))})
 })
 
